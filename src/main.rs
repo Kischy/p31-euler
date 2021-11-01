8b5239736473b293b8990866bcc4d45f20246d8e
Christian Kischy Haege <kischy@gmx.de>
2021-11-01 19:46:56 +0100

    Removed old variant and added blank file

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,10 @@
+mod exchange_counter;
+use exchange_counter::ExchangeCounter;
+
 fn main() {
     println!("Calculation started");
-    let answer_p31: u128 = 1;
+
+    let answer_p31: u128 = 0;
 
     println!(
         "The answer to problem 31 of project Euler is {}.",
